357c74fe58c8f7ed18d61469bc8097e609b5d1fb
Alvaro Garcia <alvaro.gargon.4@educa.jcyl.es>
2025-02-28 10:56:24 +0100

    Correfido

--- Ej1.java
+++ Ej1.java
@@ -3,7 +3,7 @@ public class Ej1{
 	public static void main(String[] args){
 		Scanner teclado=new Scanner (System.in);
 		int edad;
-		Syste.out.println("Dime tu edad: ");
+		System.out.println("Dime tu edad: ");
 		edad=teclado.nextInt();
 		if (edad>17)
 		{
