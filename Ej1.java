a90ecd32aa1d2f22e7c25c6fb77e4e2de1ce9c35
Alvaro Garcia <alvaro.gargon.4@educa.jcyl.es>
2025-02-28 10:42:59 +0100

    fallo

--- Ej1.java
+++ Ej1.java
@@ -3,7 +3,7 @@ public class Ej1{
 	public static void main(String[] args){
 		Scanner teclado=new Scanner (System.in);
 		int edad;
-		System.out.println("Dime tu edad: ");
+		Syste.out.println("Dime tu edad: ");
 		edad=teclado.nextInt();
 		if (edad>17)
 		{
